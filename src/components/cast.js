c66af57217e6677d73d964e4767a42a601eac83f
haishanh <haishanhan@gmail.com>
2016-07-12 20:24:08 +0800

    Add a component

---
+++ src/components/cast.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+/*
+alt: "https://movie.douban.com/celebrity/01234",
+avatars: {
+  large: xx,
+  medium: yy,
+  small: zz
+}
+id: '01234',
+name: 'Jon Snow'
+ */
+
+
+export default const Cast = props => {
+  return null;
+}
+
