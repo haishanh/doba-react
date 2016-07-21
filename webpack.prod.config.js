5b2ae9ed1c4f28bc93f9c4896dea65950acf1e73
haishanh <haishanhan@gmail.com>
2016-07-22 00:03:14 +0800

    Add webpack config for production

---
+++ webpack.prod.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+const devConfig = require('./webpack.config');
+
+const plugins = [
+  new webpack.optimize.DedupePlugin(),
+  new webpack.optimize.UglifyJsPlugin({
+    minimize: true,
+    compress: {
+      warnings: false
+    }
+  }),
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  })
+];
+
+
+// devConfig.devtool = 'cheap-module-source-map';
+devConfig.devtool = 'source-map';
+devConfig.plugins = plugins;
+
+
+module.exports = devConfig;
