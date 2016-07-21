8ca743fe17b58072ff8516dbb55a69ec63875dcf
haishanh <haishanhan@gmail.com>
2016-07-21 21:35:30 +0800

    Using clean hash history

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, hashHistory } from 'react-router';
+import { Router, useRouterHistory, hashHistory } from 'react-router';
+import { createHashHistory } from 'history';
 
 import routes from './routes';
 
 import './scss/main.scss';
 
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
+
 ReactDOM.render(
-  <Router history={hashHistory}>
+  <Router history={appHistory}>
     {routes}
   </Router>,
   document.getElementById('app')
